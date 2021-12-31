cf91d38f7540dd2e108ea1894188c217cc49b6f4
xiangang <xiangang12202@gmail.com>
2021-12-31 18:31:10 +0800

    尝试搭建快速开发框架（未完成）

---
+++ AndroidJetpackComposeSample/LibRtmpSample/src/main/java/com/nxg/rtmpsample/pull/RtmpPull.java
@@ -0,0 +1,7 @@
+package com.nxg.rtmpsample.pull;
+
+/**
+ * Rtmp拉流
+ */
+public class RtmpPull {
+}
---
+++ AndroidJetpackComposeSample/buildSrc/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    `kotlin-dsl`
+}
+repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
+    maven("https://maven.aliyun.com/repository/public")
+}
