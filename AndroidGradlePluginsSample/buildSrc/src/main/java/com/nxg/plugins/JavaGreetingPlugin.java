8e73d13559286d5cd1813a77232b515222c8ebf3
xiangang <xiangang12202@gmail.com>
2022-02-10 13:30:24 +0800

    提交在buildSrc中使用Groovy开发插件的示例

--- AndroidGradlePluginsSample/buildSrc/src/main/java/com/nxg/plugins/JavaGreetingPlugin.java
+++ AndroidGradlePluginsSample/buildSrc/src/main/java/com/nxg/plugins/JavaGreetingPlugin.java
@@ -7,6 +7,6 @@ public class JavaGreetingPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         System.out.println("JavaGreetingPlugin(buildSrc) ---> apply");
-        project.task("hello").doLast(task -> System.out.println("Hello from the com.nxg.plugins.JavaGreetingPlugin(buildSrc)"));
+        project.task("helloJava").doLast(task -> System.out.println("Hello from the com.nxg.plugins.JavaGreetingPlugin(buildSrc)"));
     }
 }
