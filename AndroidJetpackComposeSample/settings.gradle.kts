dfb1b5ed8bd967a5020f92905ce6497838ccbf83
xiangang <xiangang12202@gmail.com>
2023-04-02 11:39:08 +0800

    fix：修改编译不通过的问题

---
+++ AndroidJetpackComposeSample/settings.gradle.kts
@@ -0,0 +1,38 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+        maven { setUrl("https://jitpack.io") }
+    }
+}
+rootProject.name = "AndroidJetpackComposeSample"
+include(":app")
+include(":FastMvvm")
+include(":CommonLib")
+include(":CommonUI")
+include(":CommonUtils")
+include(":FFmpegMobile")
+include(":RtmpMobile")
+include(":YuvUtil")
+include(":AcodecMobile")
+include(":AudioRecordUtils")
+include(":WebrtcMobile")
+include(":SocketIOMobile")
+include(":IM")
+include(":FFmpegStudyComponent")
+include(":SocketIOStudyComponent")
+include(":UserComponent")
+include(":SettingComponent")
+include(":NoticeComponent")
+include(":CommonUIComponent")
